63e0e3dafdbd84abde0f7312f7cba845721478ae
alya <sitinooralya01@gmail.com>
2025-03-26 13:17:18 +0800

    week02

---
+++ week02.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5
+    }
+];
+
+// show the data in the console
+console.log(drivers);
+
+// TODO: show all the drivers name in the console
+
+// TODO: add additional driver to the drivers array
+
+async function main() {}
